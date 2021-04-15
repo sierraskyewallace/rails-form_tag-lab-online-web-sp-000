3b7af673314daf72c9c77122056063f218996c0c
sierraskyewallace <sskwallace@gmail.com>
2021-04-15 14:39:43 -0700

    commit tree 8fca33bfdf5f9b15aea8b89badc7121f69db4d23

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,11 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
-
+  def new
+    @student = Student.last
+  end
+  def create 
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to new_student_path
+  end
 end
